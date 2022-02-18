f127794ea5826ed2bae443749ee41b11ba15beab
victor castro <victor_castro_01@hotmail.com>
2022-02-18 07:00:24 -0500

    Basic Route completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,16 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map( director => {
+      return <div>
+        <h2>Name: {director.name}</h2>
+        Movies: <ul>{director.movies.map( dir => <li>{dir}</li>)}</ul>
+      </div>
+    })}</div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { movies } from "../data";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
+function Movies(){
+
+  //console.log(movies)
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movies.map(movie => {
+      return <div>
+        <h2>Title: {movie.title}</h2>
+        <p>Time: {movie.time}</p>
+        Movies: <ul>{movie.genres.map(genre => <li>{genre}</li>)}</ul>
+      </div>
+  })}
+  </div>)
 }
 
 export default Movies;
